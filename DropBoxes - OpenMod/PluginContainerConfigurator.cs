f8199490206fdf790663deb4f045de3d36586e66
Stephen White <iamsilk3495@gmail.com>
2021-06-13 18:45:57 -0300

    Configure db context via plugin container configurator

---
+++ DropBoxes - OpenMod/PluginContainerConfigurator.cs
@@ -0,0 +1,19 @@
+﻿extern alias JetBrainsAnnotations;
+using DropBoxes.Database;
+using JetBrainsAnnotations::JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using OpenMod.API.Plugins;
+using OpenMod.EntityFrameworkCore.Extensions;
+
+namespace DropBoxes
+{
+    [UsedImplicitly]
+    public class PluginContainerConfigurator : IPluginContainerConfigurator
+    {
+        public void ConfigureContainer(IPluginServiceConfigurationContext context)
+        {
+            context.ContainerBuilder.AddEntityFrameworkCoreMySql();
+            context.ContainerBuilder.AddDbContext<DropBoxesDbContext>(ServiceLifetime.Transient);
+        }
+    }
+}
