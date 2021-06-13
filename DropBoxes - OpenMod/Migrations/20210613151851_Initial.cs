4fef49ad28bc9caef8021bcaa9019a470b1ffb42
Stephen White <iamsilk3495@gmail.com>
2021-06-13 18:45:42 -0300

    Added database migration

---
+++ DropBoxes - OpenMod/Migrations/20210613151851_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DropBoxes.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DropBoxes_LootBoxes",
+                columns: table => new
+                {
+                    SteamId = table.Column<ulong>(nullable: false),
+                    LootBoxId = table.Column<string>(nullable: false),
+                    Amount = table.Column<uint>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DropBoxes_LootBoxes", x => new { x.SteamId, x.LootBoxId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DropBoxes_LootBoxes");
+        }
+    }
+}
